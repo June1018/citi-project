8fa0afb69a3b9482b996b694e20bd9b6bcfa3f5b
June1018 <128202966+June1018@users.noreply.github.com>
2024-05-22 17:08:44 +0900

    .

--- KTI/syscommbuff.h
+++ KTI/syscommbuff.h
@@ -70,3 +70,131 @@
 #define FTSKIPDATA              ((char *) (ctx->cb->item[IDX_FTSKIPDATA].buf_ptr))
 
 #define SYSICOMM_SIZE           (ctx->cb->item[IDX_SYSICOMM].buf_size)
+#define SYSGWINFO_SIZE          (ctx->cb->item[IDX_SYSGWINFO].buf_size) 
+#define EXMSG1200_SIZE          (ctx->cb->item[IDX_EXMSG1200].buf_size)
+#define SYSIOUTQ_SIZE           (ctx->cb->item[IDX_SYSIOUTQ].buf_size)
+#define EXPARM_SIZE             (ctx->cb->item[IDX_EXPARM].buf_size)
+#define TCPHEAD_SIZE            (ctx->cb->item[IDX_TCPHEAD].buf_size)
+#define HOSTRECVDATA_SIZE       (ctx->cb->item[IDX_HOSTRECVDATA].buf_size)
+#define HOSTSENDDATA_SIZE       (ctx->cb->item[IDX_HOSTSENDDATA].buf_size)
+#define EXTRECVDATA_SIZE        (ctx->cb->item[IDX_EXTRECVDATA].buf_size)
+#define EXTSENDDATA_SIZE        (ctx->cb->item[IDX_EXTSENDDATA].buf_size)
+
+
+
+/* utilty macros for commbuff */
+#define info commbuff(cb) {
+    unsinged int  size;  
+    char  *buf;
+    char  *buf_pos;
+    char  *buf_end;
+    cb_label_t *label;
+
+    buf = (char *) cb->head.buf_ptr;
+    size = cb->head.buf_size;
+
+    buf_pos = buf;
+    buf_end = buf_pos + size;
+
+    while(buf_pos < buf_end){
+        label = (cb_label_t *)buf_pos;
+        SYS_DBG("label->index[%d] size[%d]", label->index, label->size);
+
+        if (label->index == -1){
+            SYS_DBG("무효화된 item을 [%d]만큼 skip처리 ", label->size);
+            buf_pos += sizeof(cb_label_t);
+            buf_pos += label->size; 
+            continue;
+        }
+
+        buf_pos += sizeof(cb_label_t);
+        buf_pos += label->size;
+    }
+}
+
+#define PRINT_SYSIOUTH(X){\
+        SYS_DBG("================================================================="); \
+        SYS_DBG("                       SYSIOUTH                                  "); \
+        SYS_DBG("================================================================="); \
+        SYS_DBG("err_code                       : [%ld]", (x)->err_code);             \
+        SYS_DBG("ovrd_type                      : [0x%x]",(x)->ovrd_type);            \
+        SYS_DBG("err_msg                        : [%s]"  ,(x)->err_msg);              \
+        SYS_DBG("================================================================="); \
+
+#define LEN_SYSIOUTH            (sizeof(sysiouth_t))
+
+
+typedef struct sysiouth_s sysiouth_t;
+struct sysiouth_s {
+    long err_code;
+    long err_type;
+    char ovrd_type;
+    char err_file_name[MAX_LEN_FILE_NAME + 1];
+    char err_msg[MAX_LEN_ERR_MSG + 1];
+};
+
+/* commbuff header 구조체 */
+typedef struct cb_label_s cb_label_t;
+struct cb_label_s {
+    long magic;
+    long index;
+    long size;
+};
+
+/* commbuff를 구성하는 header의 구조체 */
+typedef struct cb_head_s cb_head_t;
+struct cb_head_s {
+    long magic;
+    void *buf_ptr;
+    long buf_size;
+};
+
+/* commbuff를 구성하는 item의 구조체  */
+typedef struct cb_item_s cb_item_t;
+struct cb_item_s {
+    void *buf_ptr;
+    long buf_size;
+};
+
+/* commbuff 구조체 정의 */
+typedef struct commbuff_s commbuff_t;
+struct commbuff_s{
+    cb_head_t   head;
+    cb_item_t   itme[NUM_CB_ITEM]; 
+};
+
+/* -------------------------------------------------------------------------------------------- */
+int sysocbsi (commbuff_t *cb, int index, void *data, long size)
+//commbuff에 지정된 slot에 데이터 저장
+//size가 0이거나 data null이면 기존 존재하는 데이터 clear 해당 index에 이미 데이터가 존재하는 경우 size가 같으면 기존데이터 replace
+
+int sysocbai(commbuff_t *cb, int index, void *data, long size) 
+//commbuff에 지정된slot에 데이터 append한다. 
+
+int sysocbci(commbuff_t *cb, int index) 
+//commbuff에 지정된 slot에 데이터를 clear한다.
+
+void sysocbgp(commbuff_t *cb, int index)
+//commbuff에 지정된slot에buffer pointer를 얻는다.
+
+long sysocbgs(commbuff_t *cb, int index)
+//commbuff에 지정된 slot의 buffer size를 얻는다.
+ 
+int sysocbsb(commbuff_t *cb, void *buf, long size)
+//commbuff에 항목별 pointer를 set한다. 
+ 
+int sysocbdb(commbuff_t *cb, commbuff_t *dcb) 
+// Duplicate commbuff를 복사한다.
+
+int sysocbrb(commbuff_t *cb, commbuff_t *mcb)
+// Restore commbuff. 
+
+int sysocbfb(commbuff_t *cb) 
+//commbuff를 free한다.
+
+int sysocbip(commbuff_t *cb)
+//Print commbuff info
+
+int sysocbmg(void *commbuff)
+
+#endif /* _SYSCOMMBUFF_H_  */
