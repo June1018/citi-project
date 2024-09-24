2a93b7b5cc26aad3e2018b7ee530f8bf3f274bdc
June1018 <128202966+June1018@users.noreply.github.com>
2024-09-24 14:01:21 +0900

    .

--- EI/dfn0030.c
+++ EI/dfn0030.c
@@ -206,8 +206,230 @@ static int c000_tran_code_conv(dfn0030_ctx_t *ctx)
 
     rc = ex_tran_code_convrt(&exi0251x);
     if (rc == ERR_ERR) {
-        ex_syslog
+        ex_syslog(LOG_ERROR, "[APPL_DM] %s c000_tran_code_conv(): "
+                             "거래코드 변환 ERROR : tx_code[%s] kftc_tx_code[%s]"
+                             "code/msgp[%d][%s]",
+                             __FILE__, ctx->msg_type, ctx->kftc_tx_code,
+                             sys_error_code(), sys_error_msg());
+        return ERR_ERR;
     }
+    /* 거래파라미터 LOAD이후 오류 발생시 HOST로 오류 문자 전송      */
+    ctx->host_err_send = 1;
+    memcpy(ctx->host_tx_code,   exi0251x.out.tx_code, LEN_TX_CODE);
+    SYS_DBG("c000_tran_code_conv :tx_code[%s]tx_name[%s]", exi0251x.out.tx_code, exi0251x.out.tx_name);
+
+    /* ------------------------------------------------------ */
+    SYS_DBG("[c000]host_tx_code[%s]", ctx->host_tx_code);
+    /* ------------------------------------------------------ */
+    SYS_TREF;
+
+    return ERR_NONE;
+}
+/* ----------------------------------------------------------------------------------------------------------- */
+static int d000_get_corr_id(dfn0030_ctx_t *ctx)
+{
+    int                 rc  = ERR_NONE;
+    char                corr_id[LEN_HCMIHEAD_QUEUE_NAME + 1], buff[LEN_HCMIHEAD_DATA_LEN + 1];
+    hcmihead_t          hcmihead;
+
+    SYS_TRSF;
+
+    /* kti 전송용 corr_id 생성      */
+    memset(corr_id, 0x00, sizeof(corr_id));
+
+    utoick(corr_id);
+
+    SYS_DBG("corr_id[%s]", corr_id);
+    memcpy(ctx->corr_id, corr_id, LEN_HCMIHEAD_QUEUE_NAME);
+
+    /* init hcmihead    */
+    /* MQ에서 TCPHEAD = NULL 방지하기 위해 공백을 초기화  */
+    memset(&hcmihead, 0x20, sizeof(hcmihead));
+
+    hcmihead.comm_type = 'A';       /* COMMUNICATION 구분 : Sync, Async     */
+    memcpy(hcmihead.queue_name,           corr_id , LEN_HCMIHEAD_QUEUE_NAME);
+    memcpy(hcmihead.tx_code   , ctx->host_tx_code , LEN_EXMSG11000_TX_CODE );
+
+#ifdef _DEBUGS
+    PRINT_HCMIHEAD(&hcmihead);
+#endif
+
+    /* set tcp_head     */
+    rc = sysocbsi(ctx->cb, IDX_TCPHEAD, &hcmihead, sizeof(hcmihead_t));
+
+    SYS_TREF;
+
+    return ERR_NONE;
+
+}
+/* ----------------------------------------------------------------------------------------------------------- */
+static int e000_msgdf_insert(dfn0030_ctx_t *ctx)
+{
+    int                 rc  = ERR_NONE;
+    char                buff[LEN_SIZE+1];
+    exmqmsg_df_t        exmqmsg_df;
+    dfi0003f_t          dfi0003f;
+
+    SYS_TRSF;
+
+    /* init  */
+    memset(&exmqmsg_df, 0x00, sizeof(exmqmsg_df_t));
+
+    utodate1(exmqmsg_df.proc_date);
+    utotime1(exmqmsg_df.proc_time);
+    utotime2(exmqmsg_df.sys_mtime);
+    utoclck(exmqmsg_df.ilog_jrn_no);
+
+    memcpy(exmqmsg_df.chnl_code , "007", LEN_EXMQMSG_DF_CHNL_CODE);
+    memcpy(exmqmsg_df.appl_code , "06" , LEN_EXMQMSG_DF_APPL_CODE); //EXMQPARM의 APPL_CODE 05[KTI->EI]06[EI->KTI]
+
+    exmqmsg_df.io_type = 3;     /* default :3 */
+    memcpy(exmqmsg_df.msg_id  , ctx->corr_id, LEN_EXMQMSG_DF_CORR_ID);
+    memcpy(exmqmsg_df.corr_id , ctx->corr_id, LEN_EXMQMSG_DF_CORR_ID);
+
+    utodate1(exmqmsg_df.mq_date);
+    utotime1(exmqmsg_df.mq_time);
+
+    exmqmsg_df.proc_type = 0; // insert초기값 0 mq put후에 9로 업데이트 
+    memcpy(buff, &EXTRECVDATA[4], 5);
+    ctx->len = atoi(buff);
+
+    exmqmsg_df.mqlen = ctx->len + LEN_DFI0030_HDR_DATA; /* swift헤더 9자리 THDR12345        */
+    memcpy(exmqmsg_df.mqmsg, EXTRECVDATA, sysocbgs(ctx->cb, IDX_EXTRECVDATA));
+
+    /* ------------------------------------------------------ */
+    PRINT_EXMQMSG_DF(&exmqmsg_df);
+    /* ------------------------------------------------------ */
+
+    /* ------------------------------------------------------ */
+    /* EXMQMSG_DF INSERT                                      */
+    /* ------------------------------------------------------ */
+    EXEC SQL INSERT INTO EXMQMSG_DF (
+                        PROC_DATE                 /* [8] 처리일자    */
+                      , CHNL_CODE                 /* [3] 채널코드    */
+                      , APPL_CODE                 /* [2] APPL_CODE */
+                      , IO_TYPE                   /* [1] IO_TYPE   */
+                      , MSG_ID                    /* [24]메세지ID    */
+                      , CORR_ID                   /* [20]CORR ID   */
+                      , PROC_TIME                 /*  [6]처리시간    */
+                      , RSPN_FLAG                 /*  [1]처리결과    */
+                      , ILOG_JRN_NO               /* [27]ILOGJRNNO */
+                      , SYS_MTIME                 /*  [6]SYS시간    */
+                      , PROC_TYPE                 /*  [1]처리구분    */
+                      , ERR_CODE                  /*  [7]에러코드    */
+                      , ERR_MSG                   /*[100]에러메세지   */
+                      , MQ_DATE                   /* [8] MQ일자     */
+                      , MQ_TIME                   /* [8] MQ시간     */
+                      , MQLEN                     /* [5] MQ길이     */
+                      , MQMSG                     /* CLOB:MQ메세지   */
+                    )VALUES (
+                        :exmqmsg_df.proc_date
+                       ,:exmqmsg_df.chnl_code
+                       ,:exmqmsg_df.appl_code
+                       ,:exmqmsg_df.io_type 
+                       ,:exmqmsg_df.msg_id
+                       ,:exmqmsg_df.corr_id
+                       ,:exmqmsg_df.proc_time
+                       ,:exmqmsg_df.rspn_flag 
+                       ,:exmqmsg_df.ilog_jrn_no
+                       ,:exmqmsg_df.sys_mtime
+                       ,:exmqmsg_df.proc_type
+                       ,:exmqmsg_df.err_code
+                       ,:exmqmsg_df.err_msg
+                       ,:exmqmsg_df.mq_date
+                       ,:exmqmsg_df.mq_time
+                       ,:exmqmsg_df.mqlen
+                       ,:exmqmsg_df.mqmsg
+                    );
+    if (SYS_DB_CHK_SUCCESS){
+        return ERR_NONE;
+    }
+    else if (SYS_DB_CHK_DUPKEY){
+        return ERR_NONE;
+    }else{
+        db_sql_error(SYS_DB_ERRORNUM, SYS_DB_ERRORSTR);
+        ex_syslog(LOG_ERROR, "[APPL_DM] %s EXMQMSG_DF(): INSERT [%d]"
+                             "해결방안 : 외화자금이체 담당자   call:msg[%s]",
+                             __FILE__, SYS_DB_ERRORNUM, SYS_DB_ERRORSTR);
+        SYS_HSTERR(SYS_NN, SYS_GENERR, "EXMQMSG_DF INSERT ERROR");
+        return ERR_ERR;
+    }
+
+    SYS_TREF;
+
+SYS_CATCH:
+
+    return rc;
+
+}
+/* ----------------------------------------------------------------------------------------------------------- */
+static int f000_host_msg_send(dfn0030_ctx_t *ctx)
+{
+    int                 rc  = ERR_NONE;
+    char                log_data[5000+1];
+    dfi0030x_t          dfi0030x;
+
+    SYS_TRSF;
+
+    /* kftc 보내는 응답 메세지       */
+    memset(&dfi0030x, 0x20, sizeof(dfi0030x_t));
+
+    memcpy(dfi0030x.hdr_data        ,&EXTRECVDATA[0]   , LEN_DFI0030_HDR_DATA   );
+    memcpy(dfi0030x.system_id       ,&EXTRECVDATA[9]   , LEN_DFI0030_SYSTEM_ID  );
+    memcpy(dfi0030x.msg_type        , "0210"           , LEN_DFI0030_MSG_TYPE   );
+    memcpy(dfi0030x.proc_code       , ctx->proc_code   , LEN_DFI0030_PROC_CODE  );
+    memcpy(dfi0030x.io_flag         , "2"              , LEN_DFI0030_IO_FLAG    );
+    memcpy(dfi0030x.status_code     , ctx->status_code , LEN_DFI0030_STATUS_CODE);
+    memcpy(dfi0030x.rspn_code       , "000"            , LEN_DFI0030_RSPN_CODE  );
+    memcpy(dfi0030x.rspn_bank_code  , "027"            , LEN_DFI0030_RSPN_BANK_CODE);
+    utodate1(dfi0030x.proc_date);
+    utotime1(dfi0030x.proc_time);
+    memcpy(dfi0030x.trace_no        , ctx->trace_no    , LEN_DFI0030_TRACE_NO   );
+    memcpy(dfi0030x.recv_filler     , "             "  , LEN_DFI0030_RECV_FILLER);  /* 예비정보 필드 10자리     */
+    memcpy(dfi0030x.msg_no          , ctx->msg_no      , LEN_DFI0030_MSG_NO     );
+    memcpy(dfi0030x.trd_data_nm     , ctx->trd_data_nm , LEN_DFI0030_TRD_DATA_NM);
+    memcpy(dfi0030x.div_tot_cnt     , ctx->div_tot_cnt , LEN_DFI0030_DIV_TOT_CNT);
+    memcpy(dfi0030x.div_sr_num      , ctx->div_sr_num  , LEN_DFI0030_DIV_SR_NUM );
+    ctx->ext_recv_len = ctx->len + LEN_DFI0030_HDR_DATA;
+
+    memcpy(&EXMSG11000->detl_area, &dfi0030x , ctx->ext_recv_len);
+
+    SYS_DBG("EXTSENDDATA[%d][%.*s]", ctx->ext_recv_len, ctx->ext_recv_len, &EXMSG11000->detl_area);
+    rc = sysocbsi(ctx->cb, IDX_EXTSENDDATA, &EXMSG11000->detl_area, ctx->ext_recv_len);
+    if (rc == ERR_ERR){
+        SYS_HSTERR(SYS_LN, SYS_GENERR, "COMMBUFF SET ERROR");
+        return ERR_ERR;
+    }
+
+    /* 대외기관 G/W에 호출하는 방식을 전달하기 위한 값 set */
+    strcpy(SYSGWINFO->func_name , "TCPDFC");                /* 대외기관 G/W[server]TCPDFS  [client]TCPDFC   */
+    SYSGWINFO->time_val     = SYSGWINFO_SAF_DFLT_TIMEOUT;
+    SYSGWINFO->call_type    = SYSGWINFO_CALL_TYPE_SAF;      /* SAF 방식 호출        */
+    SYSGWINFO->rspn_flag    = SYSGWINFO_REPLY;              /* G/W 로 부터의 응답    */
+    SYSGWINFO->msg_type     = SYSGWINFO_MSG_1500;           /* 1200전문외에는 etc전문구분 0=1200 전문 1:etc 기타 전문  */
+
+    /* 대외기관 데이터 전송     */
+    rc = sys_tpcall("SYSEXTGW_DF", ctx->cb, TPNOTRAN);
+    if (rc == ERR_ERR) {
+        SYS_HSTERR(SYS_LN, 0265200. "GW SYEXTGW_DF TPCALL ERROR");
+        ex_syslog(LOG_ERROR, "[APPL_DF] %s dfn0030: m000_ext_msg_send() ERROR %d"
+                             "해결방안 대외기관 G/W담당자 call ", __FILE__, tperrno );
+        return ERR_ERR;
+    }
+
+    rc = z100_log_insert(ctx, (char *)EXTRECVDATA, sysocbgs(ctx->cb, IDX_EXTRECVDATA), 'I', '4');       /* 개설응답 io_flag: I[개설거래]sr_flag:4[EI->KFTC]  */
+    if (rc == ERR_ERR){
+        ex_syslog(LOG_ERROR, "[APPL_DM] %.7s dfn0030: DFLOG ERROR() [UNIX->HOST]", __FILE__);
+    }
+
+    SYS_TREF;
+
+    return ERR_NONE;
+
+SYS_CATCH:
+
+    return ERR_ERR;
+
 }
 /* ----------------------------------------------------------------------------------------------------------- */
 /* ----------------------------------------------------------------------------------------------------------- */
